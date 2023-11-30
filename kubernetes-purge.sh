33b89c1168b5762f1201203b361b5b67a3ddf893
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2023-11-30 18:29:27 +0530

    Add files via upload

---
+++ kubernetes-purge.sh
@@ -0,0 +1,3 @@
+kubeadm reset
+sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*   
+sudo apt-get autoremove  
---
+++ master-kubeadm-1.sh
@@ -0,0 +1,16 @@
+#docker installation script
+sudo apt update 
+sudo apt install docker.io -y
+docker --version
+# kubdeadm installation script
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo swapoff -a
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+kubelet --version
+kubeadm --version
+kubectl --version
